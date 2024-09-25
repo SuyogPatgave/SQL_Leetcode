578a4731fc97726328ccea7f60e35a177a924152
Suyog (Edwin) Patgave <suyogpatgave@outlook.com>
2024-09-24 21:03:25 -0400

    Time: 948 ms (9.5%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-# Write your MySQL query statement below
 WITH temp AS (SELECT id, recordDate, temperature, 
 LAG(temperature) OVER (ORDER BY recordDate) AS previous_temperature,
 LAG(recordDate) OVER (ORDER BY recordDate) AS previous_date
