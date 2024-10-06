de4001441e2ad0f82bddc63a681ef36ebe8363c0
Suyog (Edwin) Patgave <suyogpatgave@outlook.com>
2024-10-05 21:15:47 -0400

    Time: 707 ms (28.31%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,9 +1,6 @@
 # Write your MySQL query statement below
-SELECT user_id, ROUND((confirmations/attempts), 2) AS confirmation_rate
-FROM (
-SELECT S.user_id AS user_id, C.time_stamp, action, SUM(CASE WHEN action='confirmed' THEN 1 ELSE 0 END) AS confirmations, COUNT(*) AS attempts
+SELECT S.user_id, COALESCE(ROUND(AVG(action='confirmed'), 2), 0) AS confirmation_rate
 FROM Signups AS S
 LEFT JOIN Confirmations AS C
 ON S.user_id=C.user_id
 GROUP BY S.user_id
-) AS temp_1
