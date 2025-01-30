2aeaaff0da1e10080d06bc1afcf6c8b9fb8cbfc2
Suyog (Edwin) Patgave <suyogpatgave@outlook.com>
2025-01-29 23:39:32 -0500

    Time: 549 ms (40.98%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,12 +1,18 @@
 # Write your MySQL query statement below
 WITH temp1 AS (SELECT requester_id AS id, COUNT(requester_id) as num
 FROM RequestAccepted
-GROUP BY requester_id),
+GROUP BY requester_id
+ORDER BY num DESC),
 
 temp2 AS (SELECT accepter_id AS id, COUNT(accepter_id) AS num
 FROM RequestAccepted
-GROUP BY accepter_id)
+GROUP BY accepter_id
+ORDER BY num DESC)
 
-SELECT requester_id AS id, COUNT(requester_id) as num
-FROM RequestAccepted
-GROUP BY requester_id
+SELECT id, SUM(num) AS num 
+FROM (SELECT * FROM temp1
+      UNION ALL
+      SELECT * FROM temp2)merge1
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1
